1f3fd271b5ed55873ba02207ec079c649b62f2f4
M.Zacky Restu A <101476167+coviidd19@users.noreply.github.com>
2022-03-31 23:02:54 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,98 @@
+const calculatorScreen = document.querySelector('.calculator-screen')
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const equalSign = document.querySelector('.equal-sign');
+const clearBtn = document.querySelector('.all-clear');
+const decimal = document.querySelector('.decimal');
+let prevNumber = '';
+let calculationOperator = '';
+let currentNumber = '';
+
+const updateScreen = (number) => {
+	calculatorScreen.value = number;
+};
+
+const inputNumber = (number) => {
+	if(currentNumber === '0'){
+		currentNumber = number;
+	}else{
+		currentNumber += number;
+	}
+};
+
+const inputOperator = (operator) => {
+	if(calculationOperator === ''){
+		prevNumber = currentNumber;
+	}
+	calculationOperator = operator;
+	currentNumber = 0;
+};
+
+numbers.forEach((number) => {
+	number.addEventListener("click", (event) => {
+		inputNumber(event.target.value)
+		updateScreen(currentNumber)
+	});
+});
+
+operators.forEach((operator) => {
+	operator.addEventListener("click", (event) => {
+		inputOperator(event.target.value)
+	});
+});
+
+equalSign.addEventListener('click', () => {
+	calculate();
+	updateScreen(currentNumber);
+});
+
+const calculate = () => {
+	let result = '';
+	switch(calculationOperator){
+		case "+":
+			result = parseFloat(prevNumber) + parseFloat(currentNumber);
+			break;
+		case "-":
+			result = prevNumber - currentNumber;
+			break;
+		case "*":
+			result = prevNumber * currentNumber;
+			break;
+		case "/":
+			result = prevNumber / currentNumber;
+			break;
+		default:
+			break;
+	}
+	currentNumber = result;
+	calculationOperator = '';
+};
+
+clearBtn.addEventListener('click', () => {
+	clearAll();
+	updateScreen(currentNumber);
+});
+
+const clearAll = () => {
+	prevNumber = '';
+	calculationOperator = '';
+	currentNumber = '0';
+	$('[id=info]').val("");
+};
+
+decimal.addEventListener('click', () => {
+	inputDecimal(event.target.value);
+	updateScreen(currentNumber);
+});
+
+inputDecimal = (dot) => {
+	if(currentNumber.includes('.')){
+		return
+	}
+	currentNumber += dot;
+};
+
+$('#percentage').on('click', function(){
+  const notif = "Maaf, fungsi persentase belum bisa digunakan sekarang, eheee ;D";
+  $('[id=info]').val(notif);
+});
